3196e701d3afb85ded6c91fddd9539136880bd0e
eddyrodd <eddyrodd8@gmail.com>
2025-09-25 11:22:10 -0400

    string1

---
+++ COP2250-JavaProgramming/COP2250/src/Week5/eg_string1.java
@@ -0,0 +1,12 @@
+package Week5;
+
+public class eg_string1 {
+    public static void main(String[] args) {
+        String a = "FloridaInternationalUniversity";
+        System.out.println(a);
+        System.out.println("The index of first o is: " + a.indexOf('o'));
+        System.out.println("The index of second o is: " + a.indexOf('o',3));
+        System.out.println("The character length of my 'a' string is: " + a.length());
+
+    }
+}
