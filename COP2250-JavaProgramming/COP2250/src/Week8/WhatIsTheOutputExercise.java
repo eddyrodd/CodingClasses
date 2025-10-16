786dd3201725cc3715195e442d03abe225760ed6
eddyrodd <eddyrodd8@gmail.com>
2025-10-16 13:05:21 -0400

    VolumeAndArea

---
+++ COP2250-JavaProgramming/COP2250/src/Week8/WhatIsTheOutputExercise.java
@@ -0,0 +1,12 @@
+package Week8;
+
+public class WhatIsTheOutputExercise {
+    public static void main(String[] args) {
+        for (int i =1; i<=2; i++) {
+            System.out.println("Hello");
+        }
+        for (int j =2; j<=3; j++){
+            System.out.println("FIU");
+        }
+    }
+}
