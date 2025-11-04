79a184bb3e835b173c26fe641918bd84cb38852a
eddyrodd <eddyrodd8@gmail.com>
2025-11-04 12:12:15 -0500

    VolumeAndArea

---
+++ COP2250-JavaProgramming/COP2250/src/Week11/eg1_array.java
@@ -0,0 +1,11 @@
+package Week11;
+
+public class eg1_array {
+    public static void main(String[] args) {
+        int [] number = {2,2,2,2};
+
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(number[i]);
+        }
+    }
+}
